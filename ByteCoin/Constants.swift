9c248ac48cd4252c3cbdf2d754c9ff7cba23b104
Garrett Keyes <garrett.keyes@gmail.com>
2024-01-23 21:31:16 -0500

    api calls working

---
+++ ByteCoin/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  ByteCoin
+//
+//  Created by Garrett Keyes on 1/23/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+let APIKEY = "7AC3C8B0-906A-4915-8CD4-E685496276DF"
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,9 +11,30 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
-    
+    func getCoinPrice(for currency:String){
+        
+        let urlString = "\(baseURL)/\(currency)?apikey=\(APIKEY)"
+        
+        if let url = URL(string:urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) {(data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                let dataAsString = String(data: data!, encoding: .utf8)
+                print(dataAsString!)
+                
+            }
+            task.resume()
+        }
+        
+    }
+    func performRequest(with urlString: String){
+        
+    }
 }
